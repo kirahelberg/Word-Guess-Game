7a48d8c0528c41ba17d7baaac17ca59aece759f3
Kira Helberg <kira.helberg@gmail.com>
2019-02-07 20:34:01 -0500

    troubleshoot javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,7 +23,7 @@ var wrongGuess = [];
 var answerArray = [];
 var remainingGuesses = 0;
 var gameStarted = false;
-var gameFinished = false;
+var gameFinished = true;
 var totalWins = 0;
 var totalLosses = 0;
 
@@ -31,29 +31,41 @@ function resetGame() {
   remainingGuesses = maxGuesses;
   gameStarted = false;
 
-  //PICKING A WORD IS NOT WORKING
+  //PICKS A NEW WORD EVERY TIME A KEY IS PRESSED
   var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
   wrongGuess = [];
   answerArray = [];
 
   //Add underscore for each letter of current word
-  for (var i = 0; i < currentWord.length; i++) {
-    answerArray.push("_");
-  }
+  var showUnderscore = () => {
+    for (var i = 0; i < currentWord.length; i++) {
+      if (currentWord[i] === " ") {
+        answerArray.push(" ");
+      } else {
+        answerArray.push("_");
+      }
+    }
+    return answerArray;
+  };
+  console.log(currentWord);
+  console.log(showUnderscore);
+  console.log(answerArray);
 }
-console.log(currentWord);
-console.log(answerArray);
 
 //User guess
 document.onkeyup = function(event) {
-  if ((gameFinished = true)) {
+  console.log(event.keyCode);
+  if (gameFinished === true) {
     resetGame();
     gameFinished = false;
   } else {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
+      console.log("hi");
       var userGuess = event.key.toLowerCase();
       gameStarted = true;
     }
+    //NOT WORKING - USERGUESS UNDEFINED??
+    console.log(userGuess);
 
     //Check if letter has been guessed
     if (wrongGuess.indexOf(userGuess) === -1) {
@@ -61,9 +73,6 @@ document.onkeyup = function(event) {
       evaluateGuess(userGuess);
     }
   }
-
-  //NOT WORKING - USERGUESS UNDEFINED??
-  console.log(userGuess);
 
   document.getElementById("totalWins").textContent = totalWins;
   document.getElementById("currentWord").textContent = "";
